e30c73a52a62eded0bbea6d3b787dd2ee7992e5b
zaloginmb <zaloginmb@company.dnevnik.ru>
2023-10-23 21:04:00 +0300

    Xml based config

--- src/main/java/com/application/ApplicationRunner.java
+++ src/main/java/com/application/ApplicationRunner.java
@@ -1,23 +1,14 @@
 package com.application;
 
 import com.application.spring.database.pool.ConnectionPool;
-import com.application.spring.database.repository.CompanyRepository;
-import com.application.spring.database.repository.UserRepository;
-import com.application.spring.ioc.Container;
-import com.application.spring.service.UserService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        var container = new Container();
-//        var connectionPool = new ConnectionPool();
-//        var userRepository = new UserRepository(connectionPool);
-//        var companyRepository = new CompanyRepository(connectionPool);
-//        var userService = new UserService(userRepository, companyRepository);
+        var context = new ClassPathXmlApplicationContext("application.xml");
+        var connectionPool = context.getBean("pool2", ConnectionPool.class);
+        System.out.println(connectionPool);
 
-        var connectionPool = container.get(ConnectionPool.class);
-        var userRepository = container.get(UserRepository.class);
-        var companyRepository = container.get(CompanyRepository.class);
-        var userService = container.get(UserService.class);
     }
 }
